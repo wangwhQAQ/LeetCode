f9eefb50503ee39167fe47d62bd9598fc5d4d981
wwh <710619693@qq.com>
2023-01-15 15:32:32 +0800

    605

--- 605. 种花问题/src/Solution33.java
+++ 605. 种花问题/src/Solution33.java
@@ -1,5 +1,30 @@
 class Solution33 {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int old1 = -2;
+        int f = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0){
+                if ( i == flowerbed.length-1){
+                    int len = i+2 - old1;
+                    f += (len-2)/2;
+                    old1 = i;
+                }
+            } else {
+                int len = i - old1;
+                f += (len-2)/2;
+                old1 = i;
+            }
+        }
 
+        if (f >= n){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution33 solution33 = new Solution33();
+        solution33.canPlaceFlowers(new int[]{1,0}, 1);
     }
 }
