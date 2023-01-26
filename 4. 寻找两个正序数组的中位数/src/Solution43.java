21090e21a7db9803d8f1277385da2c751f500189
wwh <710619693@qq.com>
2023-01-26 15:38:59 +0800

    43 不会

--- 4. 寻找两个正序数组的中位数/src/Solution43.java
+++ 4. 寻找两个正序数组的中位数/src/Solution43.java
@@ -1,9 +1,23 @@
 class Solution43 {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int l1 = 0, r1 = nums1.length -1;
-        int l2 = 0, r2 = nums2.length -1;
-        int k = (nums1.length + nums2.length)/2;
-        return 0.0;
+        int len1 = nums1.length, len2 = nums2.length;
+        if (len2 > len1){
+            int[] tempNums = nums1;
+            nums1 = nums2;
+            nums2 = tempNums;
+        }
 
+        len1 = nums1.length;
+        len2 = nums2.length;
+
+        int totalLeft = (len1 + len2 +1) /2;
+
+        return 0d;
+    }
+
+    public int findIndex2(int[] nums1, int[] nums2, int totalLeft, int index2){
+        int index1 = totalLeft-index2;
+
+        return 0;
     }
 }
